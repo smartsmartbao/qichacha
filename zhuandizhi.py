d27d73c96cc0fbd720d17bcb3022f113ab741158
smartbao <463230674@qq.com>
2018-09-29 09:20:57 +0800

    Update zhuandizhi.py

--- zhuandizhi.py
+++ zhuandizhi.py
@@ -4,6 +4,8 @@ import json
 import re
 a=[]
 b=[]
+tag=0
+column=['ids','fenlei_id','name','lawstatus','operationDesc','lawStatusDate','gongkai_riqi','gongkai_num','zhuanli_status','shenqing_riqi','faming_person','shenqing_person','adds','zhaiyao','g_lat','g_lng','lat','lng','address','province','city','district']
 gaode='http://restapi.amap.com/v3/geocode/geo?address={}&output=json&key=dd0bd86982932cc1a7a337cc15d415fc'
 pattern=re.compile(r'\d{6}(.*)')
 # with open(r'E:\python\网易云数据分析\专利爬取\example.csv','r') as f:
@@ -19,6 +21,8 @@ with open(r'test.csv','r',encoding='utf-8') as file:
     with open('test2.csv','a+',newline='',encoding='utf-8-sig')as f:
         writer=csv.writer(f)
         for row in rows:
+            if tag==0:
+                writer.writerow(column)
             try:
                 s=row[12]
                 if s[0].isdigit():
@@ -28,6 +32,7 @@ with open(r'test.csv','r',encoding='utf-8') as file:
                     province = location['province']
                     city = location['city']
                     district=location['district']
+                    row.append(address)
                     row.append(province)
                     row.append(city)
                     row.append(district)
@@ -46,9 +51,11 @@ with open(r'test.csv','r',encoding='utf-8') as file:
                     print('插入成功')
             except:
                 print('error')
+            tag=tag+1
         # for  i in a:
         #     b=[]
         #     r=json.loads(requests.get(gaode.format(i)).text)
         #     location=r["geocodes"][0]['location'].split(',')
         #     lng=location[0]
         #     lat=location[1]
+
