84857a6c7326ab9b5ff77ebd9481b4cb2f20d72a
smartbao <463230674@qq.com>
2019-07-02 16:45:04 +0800

    Update qichacha0701.py

--- qichacha0701.py
+++ qichacha0701.py
@@ -21,7 +21,7 @@ HEADER={
 def read_xlsx(dir,sheetname):
     wb=load_workbook(dir)
     wls=wb[sheetname]
-    cs=wls['A1:A1']
+    cs=wls['A1:A3']
     return cs
 #爬取企业详情页信息
 def  laoqu(key):
@@ -37,116 +37,107 @@ def  laoqu(key):
         if r.cookies.get_dict():
             s.cookies.update(r.cookies)
         soup = BeautifulSoup(response, 'lxml')
-        resl = soup.select('#search-result tr')[0]
-        name = resl.select('a')[0].text
-        href = resl.select('a')[0]['href']
+        try:
+            resl = soup.select('#search-result tr')[0]
+            name = resl.select('a')[0].text
+            href = resl.select('a')[0]['href']
             # print(href)
-        detail_url = 'https://www.qichacha.com' + href
-        b.append(key)
-        b.append(name)
-        detail = requests.get(detail_url, headers=HEADER).text
-        detail_soup = BeautifulSoup(detail, 'lxml')
-        try:
-            zyname = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(8) td:nth-of-type(2)')[
-                0].text.replace('\n', '').replace('                                    ', '').replace(
+            detail_url = 'https://www.qichacha.com' + href
+            b.append(key)
+            b.append(name)
+            detail = requests.get(detail_url, headers=HEADER).text
+            detail_soup = BeautifulSoup(detail, 'lxml')
+            try:
+                zyname = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(8) td:nth-of-type(2)')[0].text.replace('\n', '').replace('                                    ', '').replace(
                 '                            ', '')
-        except:
-            zyname='error'
-        try:
-            tyshbm = "'" + detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(3) td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            tyshbm='error'
-        try:
-            create_time = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(2) td:nth-of-type(4)')[
-                0].text.strip('\n                ')
-        except:
-            create_time='error'
-        try:
-            jyzc = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(2) td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            jyzc='error'
-        try:
-            hz_time = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(6) td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            hz_time='error'
-        try:
-            sshy = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(4)')[
-                0].text.strip('\n                ')
-        except:
-            sshy='error'
-        try:
-            gslx = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            gslx='error'
-        try:
-            ssdq = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(7) td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            ssdq='error'
-        try:
-            qydz = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(10)  td:nth-of-type(2)')[
-                0].text.strip('\n                ').strip('\n                 查看地图  附近公司').strip('\n                 查看地图  附近企业')
-        except:
-            qydz='error'
-        try:
-            cbrs = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(8) td:nth-of-type(4)')[
-                0].text.strip('\n                ')
-        except:
-            cbrs='error'
-        try:
-            zczb = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(1)  td:nth-of-type(2)')[
-                0].text.strip('\n                ')
-        except:
-            zczb='error'
-        try:
-            jgdm =detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(4) td:nth-of-type(4)')[0].text.strip('\n                ')
-        except:
-            jgdm='error'
-        try:
-            gslx =detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(2)')[0].text.strip('\n                ')
-        except:
-            gslx='error'
-        fenzhi_tr=detail_soup.select("section#Subcom table tr")
-        try:
-            for tr in fenzhi_tr:
-                a=tr.select('a')
-                for span in a:
-                    name=span.text
-                    fenzhi.append(name)
-        except:
-            fenzhi=[]
-        gudong_h3=detail_soup.select("section#partnerslist table tr h3")
-        try:
-            for tr in gudong_h3:
-                h3=tr.text
-                gudong.append(h3)
-            print(gudong)
-        except:
-            gudong=[]
-        dwtz=detail_soup.select('#partnerslist .ntable ntable-odd npth nptd')
-        print (dwtz)
-        b.append(zyname)
-        b.append(tyshbm)
-        b.append(create_time)
-        b.append(jyzc)
-        b.append(hz_time)
-        b.append(sshy)
-        b.append(gslx)
-        b.append(ssdq)
-        b.append(qydz)
-        b.append(cbrs)
-        b.append(zczb)
-        b.append(jgdm)
-        b.append(gslx)
-        b.append(fenzhi)
-        b.append(gudong)
-        csv_file.writerow(b)
-        print(b)
-        
+            except:
+                zyname='error'
+            try:
+                tyshbm = "'" + detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(3) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                tyshbm='error'
+            try:
+                create_time = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(2) td:nth-of-type(4)')[0].text.strip('\n                ')
+            except:
+                create_time='error'
+            try:
+                jyzc = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(2) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                jyzc='error'
+            try:
+                hz_time = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(6) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                hz_time='error'
+            try:
+                sshy = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(4)')[0].text.strip('\n                ')
+            except:
+                sshy='error'
+            try:
+                gslx = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                gslx='error'
+            try:
+                ssdq = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(7) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                ssdq='error'
+            try:
+                qydz = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(10)  td:nth-of-type(2)')[0].text.strip('\n                ').strip('\n                 查看地图  附近公司').strip('\n                 查看地图  附近企业')
+            except:
+                qydz='error'
+            try:
+                cbrs = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(8) td:nth-of-type(4)')[0].text.strip('\n                ')
+            except:
+                cbrs='error'
+            try:
+                zczb = detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(1)  td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                zczb='error'
+            try:
+                jgdm =detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(4) td:nth-of-type(4)')[0].text.strip('\n                ')
+            except:
+                jgdm='error'
+            try:
+                gslx =detail_soup.select('#Cominfo table:nth-of-type(2) tr:nth-of-type(5) td:nth-of-type(2)')[0].text.strip('\n                ')
+            except:
+                gslx='error'
+            fenzhi_tr=detail_soup.select("section#Subcom table tr")
+            try:
+                for tr in fenzhi_tr:
+                    a=tr.select('a')
+                    for span in a:
+                        name=span.text
+                        fenzhi.append(name)
+            except:
+                fenzhi=[]
+            gudong_h3=detail_soup.select("section#partnerslist table tr h3")
+            try:
+                for tr in gudong_h3:
+                    h3=tr.text
+                    gudong.append(h3)
+                print(gudong)
+            except:
+                gudong=[]
+            dwtz=detail_soup.select('#partnerslist .ntable ntable-odd npth nptd')
+            print (dwtz)
+            b.append(zyname)
+            b.append(tyshbm)
+            b.append(create_time)
+            b.append(jyzc)
+            b.append(hz_time)
+            b.append(sshy)
+            b.append(gslx)
+            b.append(ssdq)
+            b.append(qydz)
+            b.append(cbrs)
+            b.append(zczb)
+            b.append(jgdm)
+            b.append(gslx)
+            b.append(fenzhi)
+            b.append(gudong)
+            csv_file.writerow(b)
+            print(b)
+        except Exception as e:
+            print (e)
 if __name__=='__main__':
     c=read_xlsx('test.xlsx','Sheet1')
     for i in c:
